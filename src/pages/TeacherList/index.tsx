a862fb87724da5d8aca723830e0313a3c0b86c88
Joao-rangel <joao.vran@gmail.com>
2020-08-08 19:33:23 -0300

    study html creation

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import backIcon from '../../assets/images/icons/back.svg';
+import logoImg from '../../assets/images/logo.svg';
 
 function TeacherList() {
   return (
-    <h1>Teachers123</h1>
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+
+        <div className="header-content">
+          <strong>Encontre seu proffy abaixo!</strong>
+        </div>
+      </header>
+    </div>
   )
 }
 
